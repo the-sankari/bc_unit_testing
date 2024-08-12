7f3b8d0f97da39282ce89393320b694546800930
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-08-12 12:05:37 +0300

    Describe group testing

--- lesson_1/1_demotest/__tests__/demo.test.js
+++ lesson_1/1_demotest/__tests__/demo.test.js
@@ -24,10 +24,33 @@ describe("This is group A", () => {
   beforeEach(() => {
     console.log("beforeEach: ", "run before each test in group A");
   });
+
+  afterEach(() => {
+    console.log("afterEach: ", "run after each test in group A");
+  });
   test("first test in group A", () => {
     console.log("first test in group A");
   });
   test("second test in group A", () => {
     console.log("second test in group A");
+  });
+});
+
+describe("This is group B", () => {
+  describe("This is group B1 in B", () => {
+    test("test 1 in B", () => {
+      console.log("test 1 in B1");
+    });
+    test("test 2 in B", () => {
+      console.log("test 2 in B1");
+    });
+  });
+  describe("This is group B2 in B", () => {
+    test("test 3 in B", () => {
+      console.log("test 1 in B2");
+    });
+    test("test 4 in B", () => {
+      console.log("test 2 in B2");
+    });
   });
 });
