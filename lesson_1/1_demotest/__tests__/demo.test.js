e89e1b959128ace708a281c7197261d07318d295
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-08-12 10:49:05 +0300

    beforeAll and afterAll methods

--- lesson_1/1_demotest/__tests__/demo.test.js
+++ lesson_1/1_demotest/__tests__/demo.test.js
@@ -1,5 +1,21 @@
-'use strict'
+"use strict";
 
-test('This is the first test', ()=>{
-    console.log('First test');
+test("This is the first test", () => {
+  console.log("First test");
 });
+
+test("it should be 1+1=2", () => {
+  expect(1 + 1).toBe(2);
+});
+
+test('test 3 ', () => {
+  console.log('Test 3');
+})
+
+beforeAll(()=>{
+    console.log('beforeAll:', 'init before all tests');
+})
+
+afterAll(()=>{
+    console.log('afterAll:', 'cleaning after all tests');
+})
